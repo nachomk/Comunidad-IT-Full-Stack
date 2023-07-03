8da1642d8e1a1e9a7e4ce8d97f18054f64586792
Nacho <nacho.mk@hotmail.com>
2023-07-03 11:46:06 -0300

    tarea de bucles terminada

--- Tarea Bucles/tarea-bucles.js
+++ Tarea Bucles/tarea-bucles.js
@@ -1,12 +1,12 @@
 // 1) Escribe un codigo que recorra los números del 1 al 10 usando un bucle for. Por cada número, verifica si es par o impar utilizando una estructura condicional if. Imprime un mensaje indicando si el número es par o impar.
 
-for (i=1; i<= 10; i++ ) {
-    if (i%2 == 0) {
-        console.log(`${i} es par`)
-    } else {
-        console.log(`${i} es impar`)
-    }
-}
+// for (i=1; i<= 10; i++ ) {
+//     if (i%2 == 0) {
+//         console.log(`${i} es par`)
+//     } else {
+//         console.log(`${i} es impar`)
+//     }
+// }
 
 // 2) Guardar un numero en una variable. Utiliza un bucle for para recorrer los números del 1 al 10 y muestra la tabla de multiplicar del número guardado. Utiliza un condicional if para verificar si el número es válido (entre 1 y 10).
 
