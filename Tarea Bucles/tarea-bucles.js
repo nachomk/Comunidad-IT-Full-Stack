461a93d9cbe3f10ac640b66009d313b718a36452
nachomk <128156229+nachomk@users.noreply.github.com>
2023-07-04 18:52:04 -0300

    tarea-bucles.js

--- Tarea Bucles/tarea-bucles.js
+++ Tarea Bucles/tarea-bucles.js
@@ -12,7 +12,7 @@
 
 // let numero = 9
 // if (numero > 0 && numero <= 10) {
-      console.log(`La tabla del número ${numero} es:`)
+//      console.log(`La tabla del número ${numero} es:`)
 //     for (i=1; i<=10; i++) {
 //         console.log(`${numero} x ${i} = ${numero*i}`)
 //     }
