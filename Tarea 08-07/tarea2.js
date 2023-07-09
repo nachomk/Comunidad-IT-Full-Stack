39bc00f43594300c8016d9061c5414ec412ae25f
Nacho <nacho.mk@hotmail.com>
2023-07-09 19:02:09 -0300

    tarea 4

---
+++ Tarea 08-07/tarea2.js
@@ -0,0 +1,17 @@
+const persona1 = {
+    nombre: 'Pedro',
+    edad: 15
+}
+const persona2 = {
+    nombre: 'Ana',
+    edad: 29
+}
+function esMayorDeEdad(persona) {
+    if (persona.edad >= 18) {
+        console.log(`${persona.nombre} es mayor de edad`);
+    } else {
+        console.log(`${persona.nombre} es menor de edad`);
+    }
+}
+esMayorDeEdad(persona1);
+esMayorDeEdad(persona2);
