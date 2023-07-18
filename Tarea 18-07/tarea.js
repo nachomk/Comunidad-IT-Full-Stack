41cdf1b39c86afd21796caa962bfba060de56557
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-07-18 19:05:32 -0300

    actividad 8

--- Tarea 18-07/tarea.js
+++ Tarea 18-07/tarea.js
@@ -1,22 +1,23 @@
-// 8)
+// 8) 
 // const botonAgregaElemento = document.getElementById('botonElementos');
 // const listaElementos = document.getElementById('lista');
-// let textoUsuario = prompt('Ingrese un texto')
+// let numero = 0;
 
 // botonAgregaElemento.addEventListener('click', function(){
-//     let elementoLista = document.createElement('li')
+//     let elementoLista = document.createElement('li');
 //     listaElementos.appendChild(elementoLista);
-//     elementoLista.innerText = `${textoUsuario}`;
+//     elementoLista.innerText = `Elemento ${numero}`;
+//     numero++;
 // });
 
 // 8) extra
 const botonAgregaElemento = document.getElementById('botonElementos');
 const listaElementos = document.getElementById('lista');
-let numero = 0;
 
 botonAgregaElemento.addEventListener('click', function(){
+    let textoUsuario = prompt('Ingrese un texto')
     let elementoLista = document.createElement('li')
     listaElementos.appendChild(elementoLista);
-    elementoLista.innerText = `Elemento ${numero}`;
-    numero++;
+    elementoLista.innerText = `${textoUsuario}`;
 });
+
